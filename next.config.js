47b18ae3f0565f8b7456b34d2f495b38fc02e562
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-05 16:08:02 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  output: undefined, // ⬅️ VERY IMPORTANT
 };
 
 module.exports = nextConfig;
