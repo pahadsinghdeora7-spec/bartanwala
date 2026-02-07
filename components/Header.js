241794acf66fd5672c217a220ec5498600ac6d41
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-07 11:37:31 +0530

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        height: 52,
+        borderBottom: "1px solid #e5e7eb",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "bold",
+        background: "#fff",
+      }}
+    >
+      Bartanwala
+    </header>
+  );
+}
