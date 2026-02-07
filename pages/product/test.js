3e2e0d0f41ae59ab230674b88bd7b168c77e1da3
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-07 08:21:01 +0530

    Create test.js

---
+++ pages/product/test.js
@@ -0,0 +1,8 @@
+export default function TestProduct() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>PRODUCT ROUTE WORKING</h1>
+      <p>If you can see this, routing is OK.</p>
+    </div>
+  );
+}
