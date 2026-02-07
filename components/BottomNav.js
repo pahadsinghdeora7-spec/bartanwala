5b7df7b2e9bedf4511fdab11f78a6b6cc5e1eb83
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-07 11:46:26 +0530

    Update BottomNav.js

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,38 +1,14 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import {
-  FaHome,
-  FaThLarge,
-  FaShoppingCart,
-  FaClipboardList,
-  FaUser,
-} from "react-icons/fa";
 import styles from "../styles/bottomNav.module.css";
 
-const items = [
-  { name: "Home", href: "/", icon: <FaHome /> },
-  { name: "Category", href: "/category", icon: <FaThLarge /> },
-  { name: "Cart", href: "/cart", icon: <FaShoppingCart /> },
-  { name: "Orders", href: "/orders", icon: <FaClipboardList /> },
-  { name: "Account", href: "/account", icon: <FaUser /> },
-];
-
 export default function BottomNav() {
-  const router = useRouter();
-
   return (
     <nav className={styles.nav}>
-      {items.map((item) => {
-        const active = router.pathname === item.href;
-        return (
-          <Link key={item.href} href={item.href} className={styles.link}>
-            <div className={`${styles.item} ${active ? styles.active : ""}`}>
-              {item.icon}
-              <span>{item.name}</span>
-            </div>
-          </Link>
-        );
-      })}
+      <Link href="/">Home</Link>
+      <Link href="/categories">Categories</Link>
+      <Link href="/cart">Cart</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
     </nav>
   );
 }
