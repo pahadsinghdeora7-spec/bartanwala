647114086260c8f15f1f6e1ba28f47059df9fe29
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-08 22:23:09 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import "../styles/globals.css";
 import MainLayout from "../layouts/MainLayout";
 
 export default function MyApp({ Component, pageProps }) {
+  // 👉 agar admin page hai
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
+  // 👉 normal app pages
   return (
     <MainLayout>
       <Component {...pageProps} />
