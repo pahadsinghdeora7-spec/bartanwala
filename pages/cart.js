cbbc6411189fd7cb5223ea54bec0a43a583ab836
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-11 13:16:31 +0530

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -2,6 +2,7 @@ import { useCart } from "../context/CartContext";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { FaMinus, FaPlus, FaTrash } from "react-icons/fa";
+import styles from "../styles/cart.module.css";
 
 export default function CartPage() {
   const router = useRouter();
