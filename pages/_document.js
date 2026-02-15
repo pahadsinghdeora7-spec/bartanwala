07c8263c1ac3ff6b6fce3e3b1ed8104b9fce65d3
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-15 11:03:11 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,23 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 
   return (
-
     <Html lang="en">
 
       <Head>
-
-        {/* ✅ MOBILE APP VIEWPORT */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-
-        {/* ✅ APP LIKE LOOK */}
+        {/* Theme color allowed */}
         <meta name="theme-color" content="#0B5ED7" />
-
       </Head>
 
-      <body>
+      <body style={{
+        margin: 0,
+        padding: 0,
+        width: "100%",
+        maxWidth: "100%",
+        overflowX: "hidden"
+      }}>
 
         <Main />
         <NextScript />
@@ -27,7 +24,6 @@ export default function Document() {
       </body>
 
     </Html>
-
   );
 
 }
